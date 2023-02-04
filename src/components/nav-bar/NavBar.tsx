a5dd9079d6b6b2f8f621237a574de8f7c6c0b9d4
chisomije92 <chisomije92@gmail.com>
2023-02-04 01:53:13 +0100

    implement navbar with primereact

---
+++ src/components/nav-bar/NavBar.tsx
@@ -0,0 +1,97 @@
+/** @format */
+
+import React from "react";
+import { Menubar } from "primereact/menubar";
+import { InputText } from "primereact/inputtext";
+import classes from "./NavBar.module.css";
+import { Badge } from "primereact/badge";
+import { Avatar } from "primereact/avatar";
+import headshot from "../../images/headshot.jpg";
+import cosocialImg from "../../images/CO-1.png";
+//import "./NavBar.css";
+
+export default function NavBar() {
+	const items = [
+		{
+			template: (
+				<div className={`${classes.inputSearch}`}>
+					<span className="p-input-icon-left">
+						<i className="pi pi-search" />
+						<InputText
+							placeholder="Search"
+							//className="inputSearch"
+							//className={classes.inputSearch}
+						/>
+					</span>
+				</div>
+			),
+		},
+		{
+			label: "Home",
+			//style: {
+			//	color: "white",
+			//	background: "red",
+			//},
+
+			//icon: "pi pi-fw pi-file",
+		},
+		{
+			label: "Timeline",
+			//className: "",
+			//icon: "pi pi-fw pi-calendar",
+		},
+	];
+
+	const start = (
+		<div className={`mr-8 ${classes.logoContainer} hidden sm:block`}>
+			<div className="flex justify-content-between">
+				<div>
+					<img alt="logo" src={cosocialImg} height="40"></img>
+				</div>
+				<div className="-mt-2">
+					<h3>COSOCIAL</h3>
+				</div>
+			</div>
+		</div>
+	);
+
+	const end = (
+		<div className={classes.endDiv}>
+			<i
+				className="pi pi-bell p-overlay-badge mx-2"
+				style={{ fontSize: "1.4rem", cursor: "pointer" }}
+			>
+				<Badge value="2" severity="danger"></Badge>
+			</i>
+			<i
+				className="pi pi-envelope p-overlay-badge mx-2"
+				style={{ fontSize: "1.4rem", cursor: "pointer" }}
+			>
+				<Badge severity="danger"></Badge>
+			</i>
+			<i
+				className="pi pi-user p-overlay-badge mx-2"
+				style={{ fontSize: "1.4rem", cursor: "pointer" }}
+			>
+				<Badge value="5+" severity="danger"></Badge>
+			</i>
+
+			<span></span>
+			<Avatar
+				image={headshot}
+				size="large"
+				shape="circle"
+				className="ml-4 top-50"
+			/>
+		</div>
+	);
+
+	return (
+		<Menubar
+			className={`${classes.menuBar} min-w-screen border-0 text-white transition-colors transition-duration-900`}
+			start={start}
+			model={items}
+			end={end}
+		/>
+	);
+}
