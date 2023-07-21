47f3a0f4c3bdb43079c582d1506018c678b0668f
chisomije92 <chisomije92@gmail.com>
2023-07-21 11:30:10 +0100

    revise to change constants to include api url

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -1,10 +1,10 @@
-//export const urlString = 'https://cosocial-api.onrender.com/api'
-//export const urlImgString = "https://cosocial-api.onrender.com/"
-//export const socketUrl = "https://cosocial-api.onrender.com/"
+export const urlString = 'https://cosocial-api.onrender.com/api'
+export const urlImgString = "https://cosocial-api.onrender.com/"
+export const socketUrl = "https://cosocial-api.onrender.com/"
 
 
-export const urlString = 'http://localhost:8000/api'
-export const urlImgString = "http://localhost:8000/"
-export const socketUrl = "http://localhost:8000/"
+//export const urlString = 'http://localhost:8000/api'
+//export const urlImgString = "http://localhost:8000/"
+//export const socketUrl = "http://localhost:8000/"
 
 
